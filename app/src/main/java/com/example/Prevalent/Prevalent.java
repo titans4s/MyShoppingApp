55603d7418f99acfee823af120434ad4cadf1744
S.Shavin Eeswar <shavineeswar.15@gmail.com>
2020-10-03 22:18:34 +0530

    Delivery

---
+++ app/src/main/java/com/example/Prevalent/Prevalent.java
@@ -0,0 +1,13 @@
+package com.example.Prevalent;
+
+
+public class Prevalent {
+
+    public static Users currentOnlineUser;
+
+
+    public static final String UserNameKey = "UserName";
+    public static final String UserPasswordKey = "UserPassword";
+
+
+}
